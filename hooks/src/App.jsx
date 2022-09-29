dc8da0cbe46e52beeb66870277ec30afb1591ec4
vasanthkumar10 <vasizebron10@gmail.com>
2022-09-29 20:34:08 +0530

    usecontext and axios

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,16 +1,24 @@
-import { CountClass } from "./components/CountClass";
-import { CounterHooks } from "./components/CounterHooks";
-import { CountHooks } from "./components/CountHooks";
-import { StateWithArray } from "./components/StateWithArray";
-import { StateWithObject } from "./components/StateWithObject";
-import { TitleChangeClass } from "./components/TitleChangeClass";
-import { TitleChangeHooks } from "./components/TitleChangeHooks";
-import Check from "./components/Check";
-import { RefExample } from "./components/RefExample";
+// import { CountClass } from "./components/CountClass";
+// import { CounterHooks } from "./components/CounterHooks";
+// import { CountHooks } from "./components/CountHooks";
+// import { StateWithArray } from "./components/StateWithArray";
+// import { StateWithObject } from "./components/StateWithObject";
+// import { TitleChangeClass } from "./components/TitleChangeClass";
+// import { TitleChangeHooks } from "./components/TitleChangeHooks";
+// import Check from "./components/Check";
+// import { RefExample } from "./components/RefExample";
 import React, { useState } from "react";
+// import { DataFetch } from "./components/DataFetch";
+import { ComponentA } from "./components/ComponentA";
+
+export const NameContext = React.createContext();
+export const AgeContext = React.createContext();
 
 export function App() {
-  const [valid, setValid] = useState(0);
+  // const [valid, setValid] = useState(0);
+  const [name, setName] = useState("vasanth");
+  const [age, setAge] = useState(10);
+
   return (
     <>
       {/* <CountClass />
@@ -24,7 +32,14 @@ export function App() {
       <button onClick={() => setValid((prev) => prev + 1)}> */}
       {/* click = {valid}
       </button> */}
-      <RefExample />
+      {/* <RefExample /> */}
+      {/* <DataFetch /> */}
+      <NameContext.Provider value={name}>
+        <AgeContext.Provider value={age}>
+          <ComponentA />
+        </AgeContext.Provider>
+      </NameContext.Provider>
+      <p>dadasda</p>
     </>
   );
 }
