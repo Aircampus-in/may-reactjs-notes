5a2d554616edca9e9f00fe9b6a84781ea430dd66
unknown <vasizebron10@gmail.com>
2022-11-01 21:53:18 +0530

    testing cli

--- testdemo/src/components/greet/Greet.test.js
+++ testdemo/src/components/greet/Greet.test.js
@@ -1,14 +1,18 @@
 import { Greet } from "./Greet";
 import { render, screen } from "@testing-library/react";
 
-test("Greet renders properly", () => {
-  render(<Greet />);
-  const text = screen.getByText("Hello");
-  expect(text).toBeInTheDocument();
+describe("Greet", () => {
+  test("renders properly", () => {
+    render(<Greet />);
+    const text = screen.getByText("Guest");
+    expect(text).toBeInTheDocument();
+  });
 });
 
-test("Greet renders properly with name", () => {
-  render(<Greet name={"Air campus"} />);
-  const text = screen.getByText("Hello Aircampus");
-  expect(text).toBeInTheDocument();
+describe("Greet with name", () => {
+  test("renders properly with name", () => {
+    render(<Greet name={"Air campus"} />);
+    const text = screen.getByText("Hello Air campus");
+    expect(text).toBeInTheDocument();
+  });
 });
